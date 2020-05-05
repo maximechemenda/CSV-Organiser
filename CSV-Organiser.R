ac3bdb889dbc2d2a08b2982dc4758e44a76e4168
Maxime Chemenda <maxime.chemenda@gmail.com>
2020-05-05 15:09:13 +0200

    removed useless information from the data

--- CSV-Organiser.R
+++ CSV-Organiser.R
@@ -1,5 +1,12 @@
 DATA = read.csv("~/Desktop/WW_patent_2010_2019.csv", header = TRUE, sep =",", nrows=20)
 
-#only keeps the columns 5,6,8,9
-withCorrectColumns = DATA[c(5,6,8,9)]
-head(withCorrectColumns)
+#only keeps the columns REG_CODE, CTRY_CODE, APP_YEAR, IPC
+withoutUselessColumns = DATA[c(5,6,8,9)]
+
+
+longIPC = DATA[,c(9)]
+IPC = substring(longIPC,0,4) #keeps the first 4 elements of initial IPC
+
+combinedData = cbind(withoutUselessColumns, IPC)
+dataWithNewIPC = combinedData[,c(1,2,3,5)]
+head(dataWithNewIPC)
