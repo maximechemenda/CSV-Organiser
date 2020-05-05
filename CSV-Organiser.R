55d1303ada77bcb0120e29d2bf61e0feed533dbd
Maxime Chemenda <maxime.chemenda@gmail.com>
2020-05-05 18:37:48 +0200

    done with all countries file

--- CSV-Organiser.R
+++ CSV-Organiser.R
@@ -1,24 +1,38 @@
-DATA = read.csv("~/Desktop/WW_patent_2010_2019.csv", header = TRUE, sep =",", nrows=20)
+DATA = read.csv("~/Desktop/WW_patent_2010_2019.csv", header = TRUE, sep =",")
 
-#only keeps the columns REG_CODE, CTRY_CODE, APP_YEAR, IPC
+
+#only keep the columns REG_CODE, CTRY_CODE, APP_YEAR, IPC
 withoutUselessColumns = DATA[c(5,6,8,9)]
 
 
+
 longIPC = DATA[,c(9)]
-IPC = substring(longIPC,0,4) #keeps the first 4 elements of initial IPC
+IPC = substring(longIPC,0,4) #keep the first 4 elements of initial IPC
 
 library(matlab)
-countMatrix = ones(length(IPC), 1)
-count = countMatrix[,c(1)]
+YEAR_COUNTMatrix = ones(length(IPC), 1)
+YEAR_COUNT = YEAR_COUNTMatrix[,c(1)]
 
-combinedData = cbind(withoutUselessColumns, IPC, count)
+combinedData = cbind(withoutUselessColumns, IPC, YEAR_COUNT)
 dataWithNewIPC = combinedData[,c(1,2,3,5,6)]
 
 
 library(dplyr)
-#groupedData = summarise(group_by(dataWithNewIPC, REG_CODE, APP_YEAR, IPC), count= sum(count))
+groupedData = dataWithNewIPC %>% group_by(APP_YEAR, REG_CODE, CTRY_CODE, IPC) %>% summarise(YEAR_COUNT = sum(YEAR_COUNT))
 
 
-groupedData = dataWithNewIPC %>% group_by(REG_CODE, APP_YEAR, IPC) %>% summarise(count = sum(count))
+filteredData= groupedData %>%
+  filter(IPC != "")
 
-print(groupedData)
+APP_YEAR = filteredData[,c(1)]
+REG_CODE = filteredData[,c(2)]
+CTRY_CODE = filteredData[,c(3)]
+IPC = filteredData[,c(4)]
+YEAR_COUNT = filteredData[,c(5)]
+
+
+finalData = cbind(REG_CODE, CTRY_CODE, APP_YEAR, IPC, YEAR_COUNT)
+
+write.csv(finalData, file ="~/Desktop/Ordered_WW_patent_2010_2019.csv", row.names = FALSE)
+
+
