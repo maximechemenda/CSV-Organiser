5452c58308510504602b47e0cce2cc0bdede9190
Maxime Chemenda <maxime.chemenda@gmail.com>
2020-05-05 14:28:01 +0200

    commit before creating branch

--- CSV-Organiser.R
+++ CSV-Organiser.R
@@ -1,1 +1,5 @@
-head(letters)
+DATA = read.csv("~/Desktop/WW_patent_2010_2019.csv", header = TRUE, sep =",", nrows=20)
+
+#only keeps the columns 5,6,8,9
+withCorrectColumns = DATA[c(5,6,8,9)]
+head(withCorrectColumns)
