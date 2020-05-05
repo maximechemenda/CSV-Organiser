eae80c818eb185ee54389b093272d676e2039d9d
Maxime Chemenda <maxime.chemenda@gmail.com>
2020-05-05 16:55:34 +0200

    made the grouping work

--- CSV-Organiser.R
+++ CSV-Organiser.R
@@ -7,6 +7,18 @@ withoutUselessColumns = DATA[c(5,6,8,9)]
 longIPC = DATA[,c(9)]
 IPC = substring(longIPC,0,4) #keeps the first 4 elements of initial IPC
 
-combinedData = cbind(withoutUselessColumns, IPC)
-dataWithNewIPC = combinedData[,c(1,2,3,5)]
-head(dataWithNewIPC)
+library(matlab)
+countMatrix = ones(length(IPC), 1)
+count = countMatrix[,c(1)]
+
+combinedData = cbind(withoutUselessColumns, IPC, count)
+dataWithNewIPC = combinedData[,c(1,2,3,5,6)]
+
+
+library(dplyr)
+#groupedData = summarise(group_by(dataWithNewIPC, REG_CODE, APP_YEAR, IPC), count= sum(count))
+
+
+groupedData = dataWithNewIPC %>% group_by(REG_CODE, APP_YEAR, IPC) %>% summarise(count = sum(count))
+
+print(groupedData)
